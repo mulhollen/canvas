a3144bdeda6f51fd6f8d973707bbf0e403a79da1
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-06-10 18:23:16 -0500

    color picker is there and you are able to open and close it

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import reactCSS from 'reactcss'
 import CanvasDraw from "react-canvas-draw";
-import { ChromePicker } from 'react-color';
+import { SketchPicker } from 'react-color';
 
 
 class DrawingCanvas extends Component {
@@ -9,17 +10,58 @@ class DrawingCanvas extends Component {
 constructor(props) {
     super(props);
     this.state = {
-        color: "#ffc600",
-        width: 400,
-        height: 400
+        displayColorPicker: false,
+        color: "#000",
+        canvaswidth: 400,
+        canvasheight: 400
     };
 }
 
     handleChangeComplete = (color) => {
         this.setState({ color: color.hex });
     };
+    
+    handleClick = () => {
+        this.setState({ displayColorPicker: !this.state.displayColorPicker })
+    };
+
+    handleClose = () => {
+        this.setState({ displayColorPicker: false })
+    };
+
 
 render() {
+
+    const styles = reactCSS({
+        'default': {
+            color: {
+                width: '36px',
+                height: '14px',
+                borderRadius: '2px',
+                background: `${this.state.color}`,
+            },
+            swatch: {
+                padding: '5px',
+                background: '#fff',
+                borderRadius: '1px',
+                boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+                display: 'inline-block',
+                cursor: 'pointer',
+            },
+            popover: {
+                position: 'absolute',
+                zIndex: '2',
+            },
+            cover: {
+                position: 'fixed',
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px',
+            },
+        },
+    });
+
         return (
             
                 <div >
@@ -41,15 +83,26 @@ render() {
                             this.saveableCanvas.undo();
                         }}
                     >Undo</button>
-                <ChromePicker
+                {/* <SketchPicker
                     color={this.state.color}
                     onChangeComplete={this.handleChangeComplete}
-                />
+                /> */}
+
+                <div>
+                    <div style={styles.swatch} onClick={this.handleClick}>
+                        <div style={styles.color} />
+                    </div>
+                    {this.state.displayColorPicker ? <div style={styles.popover}>
+                        <div style={styles.cover} onClick={this.handleClose} />
+                        <SketchPicker color={this.state.color} onChange={this.handleChangeComplete} />
+                    </div> : null}
+
+                </div>
                 <CanvasDraw
                     ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
                     brushColor={this.state.color}
-                    canvasWidth={this.state.width}
-                    canvasHeight={this.state.height}
+                    canvasWidth={this.state.canvaswidth}
+                    canvasHeight={this.state.canvasheight}
                     />
             </div>
         );

4f9670e75f92262da51c04875d4d8bb22043d0a1
Lindsay Mulhollen <lindsaymulhollen@lindsays-mbp.attlocal.net>
2018-06-10 17:16:37 -0500

    save to local undo and clear working

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -4,12 +4,51 @@ import CanvasDraw from "react-canvas-draw";
 
 class DrawingCanvas extends Component {
 
+    state = {
+        color: "#ffc600",
+        width: 400,
+        height: 400
+    };
+   
+    static defaultProps = {
+        loadTimeOffset: 5,
+        brushSize: 6,
+        brushColor: "#444",
+        canvasWidth: 400,
+        canvasHeight: 400,
+        disabled: false
+    };
 
+    
 
 render() {
         return (
-            <div>
-                <CanvasDraw />
+            
+                <div >
+                    <button
+                        onClick={() => {
+                            localStorage.setItem(
+                                "savedDrawing",
+                                this.saveableCanvas.getSaveData()
+                            );
+                        }}
+                    >Save</button>
+                    <button
+                        onClick={() => {
+                            this.saveableCanvas.clear();
+                        }}
+                    >Clear</button>
+                    <button
+                        onClick={() => {
+                            this.saveableCanvas.undo();
+                        }}
+                    >Undo</button>
+                <CanvasDraw
+                    ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
+                    brushColor={this.state.color}
+                    canvasWidth={this.state.width}
+                    canvasHeight={this.state.height}
+                    />
             </div>
         );
     }
